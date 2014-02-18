9a12b0e05fb37c73933d6f838015c141aa8fda77
Charles-Emmanuel Dias <Charles-Emmanuel.dias@etu.upmc.fr>
2014-02-18 12:41:04 +0100

    end TP4

---
+++ tme4.m
@@ -0,0 +1,29 @@
+clear all;
+close all;
+%TME4
+source('tme4_fournis.m');
+load("PMCs/USPS/pmc_USPS_30_0.01");
+%[xapp,yapp,xtest,ytest] = dataset('Clowns',100,100,0.01);
+load("usps_napp10.dat");
+
+%{
+plot(xapp(yapp==1,1),xapp(yapp==1,2),"r+");
+	hold on;
+
+plot(xapp(yapp==-1,1),xapp(yapp==-1,2),"b+");
+
+
+%}
+ypred = [];
+%yapp(yapp==-1)=2;
+
+for i=1 : size(xapp,1)
+	pmc = put(pmc,xapp(i,:)');
+	pred = propage_avant(pmc);
+	[o, Im] = max(pred);
+	ypred = [ypred; Im];
+end
+
+nbBadClassif = sum(yapp - ypred)
+
+
