2ffded0c00b89915ecb4be061d9e3d1003a43075
charles-emmanuel <charles-emmanuel.dias@etu.upmc.fr>
2014-02-18 14:55:34 +0100

    improved

--- tme4.m
+++ tme4.m
@@ -2,10 +2,23 @@ clear all;
 close all;
 %TME4
 source('tme4_fournis.m');
-load("PMCs/USPS/pmc_USPS_25_0.01");
-%[xapp,yapp,xtest,ytest] = dataset('Clowns',100,100,0.01);
+load("PMCs/USPS/pmc_USPS_30_0.01");
+[xapp,yapp,xtest,ytest] = dataset('Clowns',100,100,0.01);
 load("usps_napp10.dat");
 
+
+
+function [ypred] = classPredPMC(data,pmc)
+	ypred = [];
+
+for i=1 : size(data,1)
+	pmc = put(pmc,data(i,:)');
+	pred = propage_avant(pmc);
+	[o, Im] = max(pred);
+	ypred = [ypred; Im];
+end
+
+endfunction
 %{
 plot(xapp(yapp==1,1),xapp(yapp==1,2),"r+");
 	hold on;
@@ -14,28 +27,17 @@ plot(xapp(yapp==-1,1),xapp(yapp==-1,2),"b+");
 
 
 %}
-ypred1 = [];
+ypred1 = classPredPMC(xapp,pmc);
+ypred1(ypred1==yapp) = 0;
+ypred1(ypred1!=0) = 1;
+nbBadClassifApp = sum(ypred1)
+
+
 %yapp(yapp==-1)=2;
 
-for i=1 : size(xapp,1)
-	pmc = put(pmc,xapp(i,:)');
-	pred = propage_avant(pmc);
-	[o, Im] = max(pred);
-	ypred1 = [ypred1; Im];
-end
-
-nbBadClassifApp = sum(yapp - ypred1) %faux marche que pour 1 & -1
-
-ypred2 = [];
-%yapp(yapp==-1)=2;
-
-for i=1 : size(xtest,1)
-	pmc = put(pmc,xtest(i,:)');
-	pred = propage_avant(pmc);
-	[o, Im] = max(pred);
-	ypred2 = [ypred2; Im];
-end
-
-nbBadClassifTest = sum(ytest - ypred2) %faux marche que pour 1 & -1
+ypred2 = classPredPMC(xtest,pmc);
+ypred2(ypred2==ytest) = 0;
+ypred2(ypred2!=0) = 1;
+nbBadClassifTest = sum(ypred2)
 
 
