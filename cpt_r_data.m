923d238bae338f9700e8d6d62345b793c9d8d050
charles-emmanuel <charles-emmanuel.dias@etu.upmc.fr>
2014-03-09 11:42:04 +0100

    started report

---
+++ cpt_r_data.m
@@ -0,0 +1,45 @@
+%Compte_rendu: Data
+clear all;
+close all;
+
+%% functionname: function description
+function [] = saveDataPS(name,xapp,yapp)
+plot(xapp(yapp==1,1),xapp(yapp==1,2),"r+");
+hold on;
+plot(xapp(yapp==-1,1),xapp(yapp==-1,2),"b*");
+
+
+	filename = [name ".eps"];
+	saveas(1,filename, "epsc");
+end
+
+figure;
+%Gaussiennes:
+[xapp,yapp,xtest,ytest] = dataset('Gaussian',100,600,0.3);
+saveDataPS("Gaussian",xapp,yapp);
+clf;
+%Clowns:
+[xapp,yapp,xtest,ytest] = dataset('Clowns',100,600,0.3);
+saveDataPS("Clowns",xapp,yapp);
+clf;
+%Checkers:
+[xapp,yapp,xtest,ytest] = dataset('Checkers',100,600,0.3);
+saveDataPS("Checkers",xapp,yapp);
+clf;
+close all;
+clear all;
+%USPS:
+load("usps_napp10.dat");
+index = 1;
+figure
+for i=[1:10:90];
+	ch = reshape(xapp(i,:),16,16)'; % remise en carre de l'image
+	subplot(3,3,index); % division de la fenêtre en 2 pour affichage ultérieur
+	hold on;
+	imagesc(ch); % affichage
+	colormap('gray'); % detail esthétique
+	index = index +1;
+end
+saveas(1,"USPS.eps", "epsc");
+
+
