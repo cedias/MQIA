3771e2ddd2df340cdc1d559e40d05d3ace66b104
Charles-Emmanuel Dias <Charles-Emmanuel.dias@etu.upmc.fr>
2014-03-25 12:44:33 +0100

    velib cont

--- velib/velib_function_app.m
+++ velib/velib_function_app.m
@@ -1,16 +1,14 @@
 %velib_function_app
 
 %chaine de markov a initialisation uniforme
-function [chaine] = initChaineMarkov(nbetats,matSequences)
+function [chaine] = initChaineMarkov(nbetats,matSequencesNorm)
 	%chaine{1}:Etats
 	%chaine{2}:Mat Transitions
 	%chaine{3}:Mat InitUniforme
 	chaine{1} = [1:nbetats];
 	chaine{2} = zeros(nbetats);
+	
+
+
 	chaine{3} = ones(nbetats,1)./nbetats;
-end
-
-function [chaine] = (chaine,matSequences)
-	chaine
-
 end
